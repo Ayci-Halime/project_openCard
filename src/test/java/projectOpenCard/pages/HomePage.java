6ebe5494f9a8d12d2cb9852314d4e04042e7429d
Sibel Tokgöz <Sibel.M.Tokgoez@gmail.com>
2024-01-09 21:58:34 +0100

    update

--- src/test/java/projectOpenCard/pages/HomePage.java
+++ src/test/java/projectOpenCard/pages/HomePage.java
@@ -1,12 +1,34 @@
 package projectOpenCard.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import projectOpenCard.utilities.DriverThreadLocal;
+
+import java.util.List;
 
 public class HomePage {
     public HomePage() {
 
+
         PageFactory.initElements(DriverThreadLocal.getDriver(), this);
     }
+    @FindBy(xpath = "//ul[@class='list-inline']/child::li")
+    public List<WebElement> ustMenu;
+    //List donduruyor; 1.index ecount 2.index wishlist 3.index shoppingCart
+
+    @FindBy(linkText = "Desktops")
+    public WebElement desktops;
+
+    @FindBy(linkText = "Laptops & Notebooks")
+    public WebElement laptopsNotebooks;
+
+    @FindBy(linkText = "Components")
+    public WebElement components;
+
+
+
+
+
 
 }
