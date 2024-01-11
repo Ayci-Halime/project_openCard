98c57e6e83c258d13f7d2da3eb3a393ecde2f3cd
Sibel Tokgöz <Sibel.M.Tokgoez@gmail.com>
2024-01-11 11:58:09 +0100

    update

--- src/test/java/projectOpenCard/tests/HomeTest.java
+++ src/test/java/projectOpenCard/tests/HomeTest.java
@@ -25,4 +25,19 @@ public class HomeTest {
         DriverThreadLocal.closeBrowser();
 
     }
+    @Test
+    public void sepeteUrunEkle(){
+        //1-Kullanici browser da URL e gider.
+        //2-Kullanici giris butonuna tiklar.
+        //3-Kullanici istedigi ürünü, arama alanina girer.
+        //4-Kullanici aradigi üründeki sepete ekle butonuna ekler.
+        //5-Kullanici sepete ekle butonuna tiklar.
+        //6-Kullanici sepetteki ürünleri görür.
+
+        DriverThreadLocal.setDriver();
+        WebDriver driver = DriverThreadLocal.getDriver();
+
+        driver.get(ConfigReader.getProperty("homepage_url"));
+
+    }
 }
